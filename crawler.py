5e7f5410964dd3b0f638408bc12f070cf2f09285
sand1k <sand1k@yandex.ru>
2019-05-03 20:13:17 +0300

    Use unidiff to find changed lines numbers in a diff.

--- crawler.py
+++ crawler.py
@@ -1,6 +1,9 @@
 from git import Repo
 import re
 import pprint
+from unidiff import PatchSet
+from functools import reduce
+import os
 
 def dump(obj):
   for attr in dir(obj):
@@ -26,10 +29,32 @@ for commit in commits[1:]:
     for diff in diff_index.iter_change_type('M'):
       if diff.a_path[-3:] == ".js":
         diffs.append(diff)
+
   if diffs:
     print(commit_message_frist_line)
     for d in diffs:
-      print(d)
+      a_path = "--- " + d.a_rawpath.decode('utf-8')
+      b_path = "+++ " + d.b_rawpath.decode('utf-8')
+      print(a_path)
+      print(b_path)
+      print(d.diff.decode('utf-8'))
+
+      # Get detailed info
+      patch = PatchSet(a_path + os.linesep + b_path + os.linesep + d.diff.decode('utf-8'))
+      print("")
+      removed_lines = []
+      added_lines = []
+      for h in patch[0]:
+        for l in h:
+          if l.source_line_no is None and l.target_line_no is not None:
+            added_lines.append(l.target_line_no)
+          elif l.source_line_no is not None and l.target_line_no is None:
+            removed_lines.append(l.source_line_no)
+
+      print("Removed lines in old file: " + str(removed_lines))
+      print("Added lines in new file: " + str(added_lines))
+      print("")
+
     input("Press Enter to continue...")
 
   prev_commit = commit
